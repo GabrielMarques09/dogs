96c8a46ddfd6dce759c895e17a9aff5edf62ce5c
Gabriel <gabrielmarrques@hotmail.com>
2020-11-23 17:41:17 -0300

    first commit

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,32 @@
+import { useContext } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+import LoginForm from './LoginFunctions/LoginForm';
+import LoginCreate from './LoginFunctions/LoginCreate';
+import LoginPasswordLost from './LoginFunctions/LoginPasswordLost';
+import LoginPasswordReset from './LoginFunctions/LoginPasswordReset';
+import { UserContext } from '../../UserContext';
+
+import styles from './Login.module.css'
+import NotFound from '../NotFound';
+
+const Login = () => {
+  const { login } = useContext(UserContext);
+
+  if(login === true) return <Navigate to="/conta" />
+  return (
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="criar" element={<LoginCreate />} />
+          <Route path="perdeu" element={<LoginPasswordLost />} />
+          <Route path="resetar" element={<LoginPasswordReset />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </section>
+  )
+};
+
+export default Login;
---
+++ src/Components/User/UserHeader.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+import UserHeaderNav from './UserHeaderNav';
+
+import { useLocation } from 'react-router-dom';
+import styles from './UserHeader.module.css';
+
+const UserHeader = () => {
+
+    const [title, setTitle] = useState('');
+    const location = useLocation();
+
+    useEffect(() => {
+        const { pathname } = location;
+        switch(pathname) {
+            case '/conta/postar':
+                setTitle('Poste sua foto');
+                break;
+            case '/conta/estatisticas':
+                setTitle('Estatísticas');
+                break;
+            default: setTitle('Minha conta');            
+        }
+    }, [location]);
+
+    return (
+        <header className={styles.header}>
+            <h1 className="title">{title}</h1>
+            <UserHeaderNav />
+        </header>
+    )
+}
+
+export default UserHeader
